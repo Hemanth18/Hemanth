51639bf8592ed6ea7237fc53ab331aedef0194cb
Hemanth18 <58596195+Hemanth18@users.noreply.github.com>
2019-12-10 13:05:44 +0530

    Exercise No.2

---
+++ Ex2.rb
@@ -0,0 +1,4 @@
+puts "I could have code like this." # and the comment after is ignored
+# you can also use comment to "disable" or comment  out a piece of code:
+# puts this wont run
+puts "This Will run."
