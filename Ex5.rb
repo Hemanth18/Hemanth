b0c21cefd9dca2a032a9f6f2a032e4fccb2be1a8
Hemanth18 <58596195+Hemanth18@users.noreply.github.com>
2019-12-13 21:30:34 +0530

    Exercise No.5

---
+++ Ex5.rb
@@ -0,0 +1,16 @@
+my_name='Hemanth'
+my_age=22
+my_height=183#in cms
+my_weight=75# in kgs
+my_eyes='Black'
+my_teeth='white'
+my_hair='Black'
+
+puts "Lets talk about #{my_name}."
+puts "He is #{my_height} cms tall."
+puts "He is #{my_weight} kgs heavy"
+puts "Actually thats not too heavy"
+puts "He got #{my_eyes} eyes and #{my_hair} hair"
+puts "His teeth are usally #{my_teeth} depending on the coffee"
+
+puts "If i add #{my_age},#{my_height},and #{my_weight} I get #{my_age+my_height+my_weight}."
